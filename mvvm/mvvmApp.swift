fa3ab9b5589b08367192ad07eaa4bea7968bd78c
Süleyman Çambel <suleyman.cambel@ekmob.com>
2023-08-19 18:58:18 +0300

    mvmm
    First Commit

---
+++ mvvm/mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmApp.swift
+//  mvvm
+//
+//  Created by Süleyman Çambel on 19.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
